eced3673ad4fafd8088586c22e5fd38e7ae12f46
Raleigh Littles III <raleighlittles@gmail.com>
2022-07-22 21:39:38 -0700

    all button reads seem to work
    the issue was that I was reading two bytes at a time, only read one, and treat each single event as a keypress/scroll -- ignoring the 2nd byte (the key RELEASED signal)

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -57,7 +57,7 @@ int main(int /* argc */, char** /* argv[] */ )
 
     while (true) {
 
-        ssize_t bytesRead = read(serialPortFileDescriptor, readBuffer.begin(), 2);
+        ssize_t bytesRead = read(serialPortFileDescriptor, readBuffer.begin(), 1);
 
         if (bytesRead < 0) {
             std::cerr << "Error reading from serial port" << std::endl;
@@ -70,8 +70,96 @@ int main(int /* argc */, char** /* argv[] */ )
             
             switch (readBuffer[0]) {
 
+                case 0x80:
+                    std::cout << "Button 7 pressed" << std::endl;
+                    break;
+
                 case 0x81:
                     std::cout << "Side button pressed" << std::endl;
+                    break;
+
+                case 0x82:
+                    std::cout << "Button 3 pressed " << std::endl;
+                    break;
+
+                case 0x83:
+                    std::cout << "Button 6 pressed" << std::endl;
+                    break;
+
+                case 0x84:
+                    std::cout << "Big center wheel moved counterclockwise" << std::endl;
+                    break;
+
+                case 0x8F:
+                    std::cout << "iPod wheel moved clockwise" << std::endl;
+                    break;
+
+                case 0x90:
+                    std::cout << "D-Pad up pressed" << std::endl;
+                    break;
+
+                case 0x91:
+                    std::cout << "D-pad down pressed" << std::endl;
+                    break;
+
+                case 0x92:
+                    std::cout << "D-pad left pressed" << std::endl;
+                    break;
+
+                case 0x93:
+                    std::cout << "D-Pad right pressed" << std::endl;
+                    break;
+
+                // End 'simple' buttons
+
+                case 0x0A:
+                    // if (readBuffer[1] == 0x0A) {
+                        std::cout << "Scroll wheel clicked" << std::endl;
+                    // }
+                    break;
+
+                case 0x09:
+                    //if (readBuffer[1] == 0x89) {
+                        std::cout << "Scroll wheel down used" << std::endl;
+                    //}
+                    break;
+
+                case 0x2A:
+                    // if (readBuffer[1] == 0xAA) {
+                        std::cout << "Button 11 pressed" << std::endl;
+                    // }
+                    break;
+
+                case 0x22:
+                    // if (readBuffer[1] == 0xA2) {
+                        std::cout << "Button A pressed" << std::endl;
+                    // }
+                    break;
+
+                case 0x23:
+                    // if (readBuffer[1] == 0xA3) {
+                        std::cout << "Button B pressed" << std::endl;
+                    // }
+                    break;
+
+                case 0x44:
+                    //if (readBuffer[1] == 0xC4) {
+                        std::cout << "Big center wheel moved clockwise" << std::endl;
+                   // }
+                    break;
+
+                case 0x49:
+                    //if (readBuffer[1] == 0xC9) {
+                        std::cout << "Scroll wheel up used" << std::endl;
+                    // }
+                    break;
+
+                case 0x4F:
+                    // if (readBuffer[1] == 0xCF) {
+                        std::cout << "iPod wheel moved counterclockwise" << std::endl;
+                    // }
+                    break;
+
 
             }
 

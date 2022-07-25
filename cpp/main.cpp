6e2c68028c03027d612d7f24a85cdb6048360eb0
Raleigh Littles III <raleighlittles@gmail.com>
2022-07-24 23:50:28 -0700

    Basic key functionality works
    Now need to implement mouse movements using Dpad

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,7 +1,7 @@
 /**
  * @file main.cpp
- * @author your name (you@domain.com)
- * @brief 
+ * @author Raleigh Littles <raleighlittles@gmail.com>
+ * @brief
  * @version 0.1
  * @date 2022-07-20
  * 
@@ -19,21 +19,9 @@
 #include <unistd.h>
 #include <iomanip>
 
-#include <linux/uinput.h>
+// Local
+#include "uinput_helper.hpp"
 
-void emit(int fd, int type, int code, int val)
-{
-   struct input_event ie;
-
-   ie.type = type;
-   ie.code = code;
-   ie.value = val;
-   /* timestamp values below are ignored */
-   ie.time.tv_sec = 0;
-   ie.time.tv_usec = 0;
-
-   write(fd, &ie, sizeof(ie));
-}
 
 int main(int /* argc */, char** /* argv[] */ )
 {
@@ -75,29 +63,9 @@ int main(int /* argc */, char** /* argv[] */ )
     std::array<uint8_t, 1> readBuffer;
 
     /// ---------- ///
-    /// Setup the virtual driver. (See example.c)
+    /// Setup the virtual driver
 
-    struct uinput_setup usetup;
-
-   int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
-
-
-   /*
-    * The ioctls below will enable the device that is about to be
-    * created, to pass key events, in this case the space key.
-    */
-   ioctl(fd, UI_SET_EVBIT, EV_KEY);
-   // ioctl(fd, UI_SET_KEYBIT, KEY_SPACE)
-   ioctl(fd, UI_SET_KEYBIT, KEY_A);
-
-   memset(&usetup, 0, sizeof(usetup));
-   usetup.id.bustype = BUS_USB;
-   usetup.id.vendor = 0x0483; /* Same vendor as default device */
-   usetup.id.product = 0xBEEF; /* Bogus product ID */
-   strcpy(usetup.name, "Custom Tourbox TBG_H Driver");
-
-   ioctl(fd, UI_DEV_SETUP, &usetup);
-   ioctl(fd, UI_DEV_CREATE);
+    const int uinputFileDescriptor = setupUinput();
 
    sleep (1);
 
@@ -118,26 +86,32 @@ int main(int /* argc */, char** /* argv[] */ )
 
                 case 0x80:
                     std::cout << "Button 7 pressed" << std::endl;
+                    generateKeyPressEvent(uinputFileDescriptor, KeyType::BUTTON_7);
                     break;
 
                 case 0x81:
                     std::cout << "Side button pressed" << std::endl;
+                    generateKeyPressEvent(uinputFileDescriptor, KeyType::SIDE_BUTTON);
                     break;
 
                 case 0x82:
                     std::cout << "Button 3 pressed " << std::endl;
+                    generateKeyPressEvent(uinputFileDescriptor, KeyType::BUTTON_3);
                     break;
 
                 case 0x83:
                     std::cout << "Button 6 pressed" << std::endl;
+                    generateKeyPressEvent(uinputFileDescriptor, KeyType::BUTTON_6);
                     break;
 
                 case 0x84:
                     std::cout << "Big center wheel moved counterclockwise" << std::endl;
+                    generateKeyPressEvent(uinputFileDescriptor, KeyType::IPOD_WHEEL_COUNTERCLOCKWISE);
                     break;
 
                 case 0x8F:
                     std::cout << "iPod wheel moved clockwise" << std::endl;
+                    generateKeyPressEvent(uinputFileDescriptor, KeyType::IPOD_WHEEL_CLOCKWISE);
                     break;
 
                 case 0x90:
@@ -158,39 +132,43 @@ int main(int /* argc */, char** /* argv[] */ )
 
                 case 0x0A:
                         std::cout << "Scroll wheel clicked" << std::endl;
+                        generateKeyPressEvent(uinputFileDescriptor, KeyType::SCROLL_WHEEL_CLICK);
                     break;
 
                 case 0x09:
                         std::cout << "Scroll wheel down used" << std::endl;
+                        generateKeyPressEvent(uinputFileDescriptor, KeyType::SCROLL_WHEEL_DOWN);
                     break;
 
                 case 0x2A:
                         std::cout << "Button 11 pressed" << std::endl;
+                        generateKeyPressEvent(uinputFileDescriptor, KeyType::BUTTON_11);
                     break;
 
                 case 0x22:
-
                         std::cout << "Button A pressed" << std::endl;
-                        emit(fd, EV_KEY, KEY_A, 1);
-                        emit(fd, EV_SYN, SYN_REPORT, 0);
-                        emit(fd, EV_KEY, KEY_A, 0);
-                        emit(fd, EV_SYN, SYN_REPORT, 0);
+                        generateKeyPressEvent(uinputFileDescriptor, KeyType::BUTTON_A);
+
                     break;
 
                 case 0x23:
                         std::cout << "Button B pressed" << std::endl;
+                        generateKeyPressEvent(uinputFileDescriptor, KeyType::BUTTON_B);
                     break;
 
                 case 0x44:
                         std::cout << "Big center wheel moved clockwise" << std::endl;
+                        generateKeyPressEvent(uinputFileDescriptor, KeyType::BIG_CENTER_WHEEL_CLOCKWISE);
                     break;
 
                 case 0x49:
                         std::cout << "Scroll wheel up used" << std::endl;
+                        generateKeyPressEvent(uinputFileDescriptor, KeyType::SCROLL_WHEEL_UP);
                     break;
 
                 case 0x4F:
                         std::cout << "iPod wheel moved counterclockwise" << std::endl;
+                        generateKeyPressEvent(uinputFileDescriptor, KeyType::IPOD_WHEEL_COUNTERCLOCKWISE);
                     break;
 
                 // Don't put a default statement here, since the bytes you'd catch would just be the 'RELEASED' version of the keypress signal 
@@ -205,12 +183,10 @@ int main(int /* argc */, char** /* argv[] */ )
         
     }
 
-    sleep(1);
-
     /// ---------- ///
     // Clean up
-    ioctl(fd, UI_DEV_DESTROY);
-    close(fd);
+
+    destroyUinput(uinputFileDescriptor);
 
 
     return 0;

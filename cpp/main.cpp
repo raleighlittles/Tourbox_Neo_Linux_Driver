2b5c53920aa4b6d8d64b674b454501cb9f278fdb
Raleigh Littles III <raleighlittles@gmail.com>
2022-07-25 00:54:26 -0700

    ran through formatter

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,28 +1,26 @@
 /**
  * @file main.cpp
  * @author Raleigh Littles <raleighlittles@gmail.com>
- * @brief
+ * @brief Entrypoint for Tourbox driver
  * @version 0.1
  * @date 2022-07-20
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
-
 #include <array>
-#include <string>
-#include <fcntl.h>
-#include <iostream>
 #include <cstring>
+#include <fcntl.h>
+#include <iomanip>
+#include <iostream>
+#include <signal.h>
+#include <string>
 #include <termios.h>
 #include <unistd.h>
-#include <iomanip>
-#include <signal.h>
 
 // Local
 #include "uinput_helper.hpp"
-
 
 // Remember, can't pass data to signals
 int gUinputFileDescriptor = 0;
@@ -33,7 +31,7 @@ void sigint_handler(sig_atomic_t /* s */)
     exit(1);
 }
 
-int main(int /* argc */, char** /* argv[] */ )
+int main(int /* argc */, char ** /* argv[] */)
 {
 
     /// ---------- ///
@@ -43,7 +41,8 @@ int main(int /* argc */, char** /* argv[] */ )
 
     const int serialPortFileDescriptor = open(serialPortFile.c_str(), O_RDWR | O_NOCTTY | O_NONBLOCK);
 
-    if (serialPortFileDescriptor == -1) {
+    if (serialPortFileDescriptor == -1)
+    {
         std::cerr << "Failed to open serial port file " << serialPortFile << std::endl;
         return 1;
     }
@@ -53,14 +52,16 @@ int main(int /* argc */, char** /* argv[] */ )
 
     term_options.c_cflag = B115200 | CS8 | CREAD;
 
-    if ((tcsetattr(serialPortFileDescriptor, TCSANOW, &term_options)) != 0) {
+    if ((tcsetattr(serialPortFileDescriptor, TCSANOW, &term_options)) != 0)
+    {
 
         std::cerr << "Failed to set termios settings";
         close(serialPortFileDescriptor);
         return 1;
     }
 
-    if ((tcflush(serialPortFileDescriptor, TCIOFLUSH)) != 0) {
+    if ((tcflush(serialPortFileDescriptor, TCIOFLUSH)) != 0)
+    {
 
         std::cerr << "Failed to flush termios settings";
         close(serialPortFileDescriptor);
@@ -80,133 +81,134 @@ int main(int /* argc */, char** /* argv[] */ )
     // Register signal handler to make sure virtual device gets cleaned up
     signal(SIGINT, sigint_handler);
 
-   sleep (1);
+    sleep(1);
 
-    while (true) {
+    while (true)
+    {
 
         ssize_t bytesRead = read(serialPortFileDescriptor, readBuffer.begin(), 1);
 
-        if (bytesRead < 0) {
+        if (bytesRead < 0)
+        {
             std::cerr << "Error reading from serial port" << std::endl;
             return 2;
         }
 
-        if (bytesRead > 0) {
+        if (bytesRead > 0)
+        {
 
             std::cout << std::hex << std::setfill('0') << std::setw(2) << readBuffer[0] << " " << readBuffer[1] << std::endl;
-            
-            switch (readBuffer[0]) {
 
-                case 0x80:
-                    std::cout << "Button 7 pressed" << std::endl;
-                    generateKeyPressEvent(gUinputFileDescriptor, KeyType::BUTTON_7);
-                    break;
+            switch (readBuffer[0])
+            {
 
-                case 0x81:
-                    std::cout << "Side button pressed" << std::endl;
-                    generateKeyPressEvent(gUinputFileDescriptor, KeyType::SIDE_BUTTON);
-                    break;
+            case 0x80:
+                std::cout << "Button 7 pressed" << std::endl;
+                generateKeyPressEvent(gUinputFileDescriptor, KeyType::BUTTON_7);
+                break;
 
-                case 0x82:
-                    std::cout << "Button 3 pressed " << std::endl;
-                    generateKeyPressEvent(gUinputFileDescriptor, KeyType::BUTTON_3);
-                    break;
+            case 0x81:
+                std::cout << "Side button pressed" << std::endl;
+                generateKeyPressEvent(gUinputFileDescriptor, KeyType::SIDE_BUTTON);
+                break;
 
-                case 0x83:
-                    std::cout << "Button 6 pressed" << std::endl;
-                    generateKeyPressEvent(gUinputFileDescriptor, KeyType::BUTTON_6);
-                    break;
+            case 0x82:
+                std::cout << "Button 3 pressed " << std::endl;
+                generateKeyPressEvent(gUinputFileDescriptor, KeyType::BUTTON_3);
+                break;
 
-                case 0x84:
-                    std::cout << "Big center wheel moved counterclockwise" << std::endl;
-                    generateKeyPressEvent(gUinputFileDescriptor, KeyType::IPOD_WHEEL_COUNTERCLOCKWISE);
-                    break;
+            case 0x83:
+                std::cout << "Button 6 pressed" << std::endl;
+                generateKeyPressEvent(gUinputFileDescriptor, KeyType::BUTTON_6);
+                break;
 
-                case 0x8F:
-                    std::cout << "iPod wheel moved clockwise" << std::endl;
-                    generateKeyPressEvent(gUinputFileDescriptor, KeyType::IPOD_WHEEL_CLOCKWISE);
-                    break;
+            case 0x84:
+                std::cout << "Big center wheel moved counterclockwise" << std::endl;
+                generateKeyPressEvent(gUinputFileDescriptor, KeyType::IPOD_WHEEL_COUNTERCLOCKWISE);
+                break;
 
-                case 0x90:
-                    std::cout << "D-Pad up pressed" << std::endl;
-                    emit(gUinputFileDescriptor, EV_REL, REL_Y, -5);
-                    emit(gUinputFileDescriptor, EV_SYN, SYN_REPORT, 0);
-                    break;
+            case 0x8F:
+                std::cout << "iPod wheel moved clockwise" << std::endl;
+                generateKeyPressEvent(gUinputFileDescriptor, KeyType::IPOD_WHEEL_CLOCKWISE);
+                break;
 
-                case 0x91:
-                    std::cout << "D-pad down pressed" << std::endl;
-                    emit(gUinputFileDescriptor, EV_REL, REL_Y, +5);
-                    emit(gUinputFileDescriptor, EV_SYN, SYN_REPORT, 0);
-                    break;
+            case 0x90:
+                std::cout << "D-Pad up pressed" << std::endl;
+                emit(gUinputFileDescriptor, EV_REL, REL_Y, -5);
+                emit(gUinputFileDescriptor, EV_SYN, SYN_REPORT, 0);
+                break;
 
-                case 0x92:
-                    std::cout << "D-pad left pressed" << std::endl;
-                    emit(gUinputFileDescriptor, EV_REL, REL_X, -5);
-                    emit(gUinputFileDescriptor, EV_SYN, SYN_REPORT, 0);
-                    break;
+            case 0x91:
+                std::cout << "D-pad down pressed" << std::endl;
+                emit(gUinputFileDescriptor, EV_REL, REL_Y, +5);
+                emit(gUinputFileDescriptor, EV_SYN, SYN_REPORT, 0);
+                break;
 
-                case 0x93:
-                    std::cout << "D-Pad right pressed" << std::endl;
-                    emit(gUinputFileDescriptor, EV_REL, REL_X, +5);
-                    emit(gUinputFileDescriptor, EV_SYN, SYN_REPORT, 0);
-                    break;
+            case 0x92:
+                std::cout << "D-pad left pressed" << std::endl;
+                emit(gUinputFileDescriptor, EV_REL, REL_X, -5);
+                emit(gUinputFileDescriptor, EV_SYN, SYN_REPORT, 0);
+                break;
 
-                case 0x0A:
-                        std::cout << "Scroll wheel clicked" << std::endl;
-                        generateKeyPressEvent(gUinputFileDescriptor, KeyType::SCROLL_WHEEL_CLICK);
-                    break;
+            case 0x93:
+                std::cout << "D-Pad right pressed" << std::endl;
+                emit(gUinputFileDescriptor, EV_REL, REL_X, +5);
+                emit(gUinputFileDescriptor, EV_SYN, SYN_REPORT, 0);
+                break;
 
-                case 0x09:
-                        std::cout << "Scroll wheel down used" << std::endl;
-                        generateKeyPressEvent(gUinputFileDescriptor, KeyType::SCROLL_WHEEL_DOWN);
-                    break;
+            case 0x0A:
+                std::cout << "Scroll wheel clicked" << std::endl;
+                generateKeyPressEvent(gUinputFileDescriptor, KeyType::SCROLL_WHEEL_CLICK);
+                break;
 
-                case 0x2A:
-                        std::cout << "Button 11 pressed" << std::endl;
-                        generateKeyPressEvent(gUinputFileDescriptor, KeyType::BUTTON_11);
-                    break;
+            case 0x09:
+                std::cout << "Scroll wheel down used" << std::endl;
+                generateKeyPressEvent(gUinputFileDescriptor, KeyType::SCROLL_WHEEL_DOWN);
+                break;
 
-                case 0x22:
-                        std::cout << "Button A pressed" << std::endl;
-                        generateKeyPressEvent(gUinputFileDescriptor, KeyType::BUTTON_A);
+            case 0x2A:
+                std::cout << "Button 11 pressed" << std::endl;
+                generateKeyPressEvent(gUinputFileDescriptor, KeyType::BUTTON_11);
+                break;
 
-                    break;
+            case 0x22:
+                std::cout << "Button A pressed" << std::endl;
+                generateKeyPressEvent(gUinputFileDescriptor, KeyType::BUTTON_A);
 
-                case 0x23:
-                        std::cout << "Button B pressed" << std::endl;
-                        generateKeyPressEvent(gUinputFileDescriptor, KeyType::BUTTON_B);
-                    break;
+                break;
 
-                case 0x44:
-                        std::cout << "Big center wheel moved clockwise" << std::endl;
-                        generateKeyPressEvent(gUinputFileDescriptor, KeyType::BIG_CENTER_WHEEL_CLOCKWISE);
-                    break;
+            case 0x23:
+                std::cout << "Button B pressed" << std::endl;
+                generateKeyPressEvent(gUinputFileDescriptor, KeyType::BUTTON_B);
+                break;
 
-                case 0x49:
-                        std::cout << "Scroll wheel up used" << std::endl;
-                        generateKeyPressEvent(gUinputFileDescriptor, KeyType::SCROLL_WHEEL_UP);
-                    break;
+            case 0x44:
+                std::cout << "Big center wheel moved clockwise" << std::endl;
+                generateKeyPressEvent(gUinputFileDescriptor, KeyType::BIG_CENTER_WHEEL_CLOCKWISE);
+                break;
 
-                case 0x4F:
-                        std::cout << "iPod wheel moved counterclockwise" << std::endl;
-                        generateKeyPressEvent(gUinputFileDescriptor, KeyType::IPOD_WHEEL_COUNTERCLOCKWISE);
-                    break;
+            case 0x49:
+                std::cout << "Scroll wheel up used" << std::endl;
+                generateKeyPressEvent(gUinputFileDescriptor, KeyType::SCROLL_WHEEL_UP);
+                break;
 
-                // Don't put a default statement here, since the bytes you'd catch would just be the 'RELEASED' version of the keypress signal 
+            case 0x4F:
+                std::cout << "iPod wheel moved counterclockwise" << std::endl;
+                generateKeyPressEvent(gUinputFileDescriptor, KeyType::IPOD_WHEEL_COUNTERCLOCKWISE);
+                break;
+
+                // Don't put a default statement here, since the bytes you'd catch would just be the 'RELEASED' version of the keypress signal
                 // (since above you're only listening to the 'PRESSED' version)
             }
 
             usleep(1000);
-
         }
 
         usleep(1000);
-        
     }
 
     // Clean up (although currently you can't get here)
     destroyUinput(gUinputFileDescriptor);
-
 
     return 0;
 }
--- cpp/uinput_helper.hpp
+++ cpp/uinput_helper.hpp
@@ -1,21 +1,21 @@
 /**
  * @file uinput_helper.hpp
- * @author your name (you@domain.com)
- * @brief 
+ * @author Raleigh Littles <raleighlittles@gmail.com>
+ * @brief Helper functions for working with Linux's `uinput` module
  * @version 0.1
  * @date 2022-07-24
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
-#include <linux/uinput.h>
-#include <sys/ioctl.h>
-#include <fcntl.h>
-#include <string.h>
-#include <unistd.h>
-#include <map>
 
+#include <fcntl.h>
+#include <linux/uinput.h>
+#include <map>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
 enum class KeyType
 {
@@ -39,36 +39,34 @@ enum class KeyType
     BUTTON_11
 };
 
-static std::map<KeyType, int> keyMap = { std::make_pair(KeyType::SIDE_BUTTON, KEY_MUTE),
-                                  std::make_pair(KeyType::SCROLL_WHEEL_UP, KEY_SCROLLUP),
-                                  std::make_pair(KeyType::SCROLL_WHEEL_DOWN, KEY_SCROLLDOWN),
-                                  std::make_pair(KeyType::SCROLL_WHEEL_CLICK, KEY_PAUSE),
-                                  std::make_pair(KeyType::BUTTON_3, KEY_ENTER),
-                                  std::make_pair(KeyType::BUTTON_A, BTN_LEFT),
-                                  std::make_pair(KeyType::BUTTON_B, BTN_RIGHT),
-                                  std::make_pair(KeyType::BUTTON_6, KEY_DOT),
-                                  std::make_pair(KeyType::BUTTON_7, KEY_MENU),
-                                  std::make_pair(KeyType::IPOD_WHEEL_CLOCKWISE, KEY_VOLUMEUP),
-                                  std::make_pair(KeyType::IPOD_WHEEL_COUNTERCLOCKWISE, KEY_VOLUMEDOWN),
-                                  std::make_pair(KeyType::BIG_CENTER_WHEEL_CLOCKWISE, KEY_BRIGHTNESSUP),
-                                  std::make_pair(KeyType::BIG_CENTER_WHEEL_COUNTERCLOCKWISE, KEY_BRIGHTNESSDOWN),
-                                  std::make_pair(KeyType::BUTTON_11, KEY_ESC)
-                                   };
+static std::map<KeyType, int> keyMap = {std::make_pair(KeyType::SIDE_BUTTON, KEY_MUTE),
+                                        std::make_pair(KeyType::SCROLL_WHEEL_UP, KEY_SCROLLUP),
+                                        std::make_pair(KeyType::SCROLL_WHEEL_DOWN, KEY_SCROLLDOWN),
+                                        std::make_pair(KeyType::SCROLL_WHEEL_CLICK, KEY_PAUSE),
+                                        std::make_pair(KeyType::BUTTON_3, KEY_ENTER),
+                                        std::make_pair(KeyType::BUTTON_A, BTN_LEFT),
+                                        std::make_pair(KeyType::BUTTON_B, BTN_RIGHT),
+                                        std::make_pair(KeyType::BUTTON_6, KEY_DOT),
+                                        std::make_pair(KeyType::BUTTON_7, KEY_MENU),
+                                        std::make_pair(KeyType::IPOD_WHEEL_CLOCKWISE, KEY_VOLUMEUP),
+                                        std::make_pair(KeyType::IPOD_WHEEL_COUNTERCLOCKWISE, KEY_VOLUMEDOWN),
+                                        std::make_pair(KeyType::BIG_CENTER_WHEEL_CLOCKWISE, KEY_BRIGHTNESSUP),
+                                        std::make_pair(KeyType::BIG_CENTER_WHEEL_COUNTERCLOCKWISE, KEY_BRIGHTNESSDOWN),
+                                        std::make_pair(KeyType::BUTTON_11, KEY_ESC)};
 
 void emit(int fileDescriptor, int type, int code, int val)
 {
-   struct input_event ie;
+    struct input_event ie;
 
-   ie.type = type;
-   ie.code = code;
-   ie.value = val;
-   /* timestamp values below are ignored */
-   ie.time.tv_sec = 0;
-   ie.time.tv_usec = 0;
+    ie.type = type;
+    ie.code = code;
+    ie.value = val;
+    /* timestamp values below are ignored */
+    ie.time.tv_sec = 0;
+    ie.time.tv_usec = 0;
 
-   write(fileDescriptor, &ie, sizeof(ie));
+    write(fileDescriptor, &ie, sizeof(ie));
 }
-
 
 void generateKeyPressEvent(int fileDescriptor, KeyType key)
 {
@@ -78,7 +76,6 @@ void generateKeyPressEvent(int fileDescriptor, KeyType key)
     emit(fileDescriptor, EV_SYN, SYN_REPORT, 0);
 }
 
-
 void registerKeyboardEvents(int fileDescriptor)
 {
     // Get the the uinput interface ready to register key and mouse events
@@ -86,7 +83,8 @@ void registerKeyboardEvents(int fileDescriptor)
 
     // These are all of the following key types that the Tourbox will be able to produce using this driver
 
-    for (const auto keyType: keyMap) {
+    for (const auto keyType : keyMap)
+    {
         ioctl(fileDescriptor, UI_SET_KEYBIT, keyType.second);
     }
 }
@@ -102,12 +100,10 @@ void registerMouseEvents(int fileDescriptor)
     ioctl(fileDescriptor, UI_SET_KEYBIT, BTN_RIGHT);
     ioctl(fileDescriptor, UI_SET_EVBIT, EV_REL);
 
-
     // Tells the uinput interface to allow mouse movements
     ioctl(fileDescriptor, UI_SET_RELBIT, REL_X);
     ioctl(fileDescriptor, UI_SET_RELBIT, REL_Y);
 }
-
 
 int setupUinput(void)
 {
@@ -125,7 +121,7 @@ int setupUinput(void)
     usetup.id.vendor = 0x0483; /* Same vendor as default device */
 
     usetup.id.product = 0xBEEF; /* Bogus product ID */
-    strcpy(usetup.name,"Custom Tourbox TBG_H Driver (Keyboard/Mouse)");
+    strcpy(usetup.name, "Custom Tourbox TBG_H Driver (Keyboard/Mouse)");
 
     ioctl(fd, UI_DEV_SETUP, &usetup);
     ioctl(fd, UI_DEV_CREATE);
